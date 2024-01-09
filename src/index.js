7ce09950964480a8fa08f24255e7b691eb23106a
Harsh Shah <hs553608@gmail.com>
2024-01-09 19:00:44 +0530

    connected to mongo Atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("harsh")
+import dotenv from 'dotenv'
+import express from 'express'
+import connectDB from './db/index.js'
+//Second appraoch
+
+dotenv.config({
+    path:"./env"
+})
+connectDB()
+
+//First approach is to connect to the database in the index.js file 
+// import express from "express";
+
+// const app=express();
+
+// ;(async()=>{
+//     try{
+//       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//       app.on('error',(err)=>{
+//         console.log(err);
+//       })
+//       app.listenerCount(process.env.PORT,()=>{
+//         console.log(`listening on port ${process.env.PORT} `)
+//       })
+//     }catch{
+//         console.log("error connecting to db")
+//     }
+// })()
+
